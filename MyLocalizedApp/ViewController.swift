aa0d9c70abfbb8ec5098817f3e524d79d3b9960d
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2016-03-06 18:48:26 -0800

    Initial commit

--- MyLocalizedApp/ViewController.swift
+++ MyLocalizedApp/ViewController.swift
@@ -20,6 +20,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func runButtonTapped(sender: AnyObject) {
+        
+        let successMessage = NSLocalizedString("signUpPage.SuccessMessage", comment: "A success message user will see when sign up is successfull.")
+        
+         let alertTitle = NSLocalizedString("signUpPage.SuccessAlertWindowTitle", comment: "Alert message title")
+        
+        let myAlert = UIAlertController(title: alertTitle, message: successMessage, preferredStyle: UIAlertControllerStyle.Alert)
+        
+        let okAction = UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil)
+        
+        myAlert.addAction(okAction)
+        
+        self.presentViewController(myAlert, animated: true, completion: nil)
+        
+    }
 
 }
 
